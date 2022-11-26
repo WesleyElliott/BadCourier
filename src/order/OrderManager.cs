7e8d350bd026e927a32fd9bd06302925e01af358
Wesley Elliott <wesleyjasonelliott@gmail.com>
2022-11-26 16:40:02 +0200

    Add order manager for generating orders
    When Space is pressed, order is generated at one of the available locations:
     - if there is a drop off available
     - if a collection spot is free

---
+++ src/order/OrderManager.cs
@@ -0,0 +1,63 @@
+using Godot;
+using System.Linq;
+
+public partial class OrderManager : Node {
+
+    [Export]
+    public Node CollectionPoints { get; private set; }
+
+    [Export]
+    public Node DropOffPoints { get; private set; }
+
+    private RandomNumberGenerator rng = new RandomNumberGenerator();
+
+    public override void _Ready() {
+        rng.Randomize();
+        // Disable all of them for now
+        foreach (DropOff dropOff in DropOffPoints.GetChildren()) {
+            dropOff.Disable();
+        }
+    }
+
+    public override void _UnhandledKeyInput(InputEvent @event) {
+        if (@event is InputEventKey eventKey) {
+            if (eventKey.Pressed && eventKey.Keycode == Key.Space) {
+                GenerateOrder();
+            }
+        }
+    }
+
+    /**
+        Order generation:
+        1. Get random drop off point
+        2. Get random collection point
+        3. Create Box instance, placed at collection point
+        4. Enable drop off point
+    */
+    private void GenerateOrder() {
+        GD.Print("[Order] Generating order...");
+        var eligibleCollectionPoints = CollectionPoints.GetChildren()
+            .Cast<Node3D>()
+            .Where(point => point.GetChildCount() == 0);
+        var eligibleDropOffPoints = DropOffPoints.GetChildren()
+            .Cast<DropOff>()
+            .Where(box => box.Visible == false);
+
+        if (eligibleDropOffPoints.Count() == 0 || eligibleCollectionPoints.Count() == 0) {
+            GD.Print("[Order] No more drop of points...");
+            return;
+        }
+
+        var collectionPoint = eligibleCollectionPoints.ElementAt(rng.RandiRange(0, eligibleCollectionPoints.Count() - 1));
+        var dropOffPoint = eligibleDropOffPoints.ElementAt(rng.RandiRange(0, eligibleDropOffPoints.Count() - 1));
+
+        GD.Print($"[Order] Collection @ {collectionPoint.Position} | Drop off @ {dropOffPoint.Position}");
+
+        var boxScene = GD.Load<PackedScene>("res://src/world/misc/box/Box.tscn");
+        Box box = (Box) boxScene.Instantiate();
+        box.DropOff = dropOffPoint;
+
+        collectionPoint.AddChild(box);
+        dropOffPoint.Enable();
+    }
+}
