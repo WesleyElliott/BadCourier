4d813134f1bc90b8c5328a71851ed2d6fde47aff
Wesley Elliott <wesleyjasonelliott@gmail.com>
2022-11-26 16:40:45 +0200

    Add a tutorial dialog

---
+++ src/tutorial/dialog/TutorialDialog.cs
@@ -0,0 +1,75 @@
+using Godot;
+
+public partial class TutorialDialog : Control {
+
+    [Export]
+    public Label MessageLabel;
+
+    private Tween tween;
+    private bool isShown = false;
+
+    public override void _UnhandledKeyInput(InputEvent @event) {
+        if (@event is InputEventKey eventKey) {
+            if (eventKey.Pressed ) {
+                if (eventKey.Keycode == Key.Key1) {
+                    if (isShown) {
+                        HideDialog();
+                        isShown = false;
+                    } else {
+                        ShowDialog();
+                        isShown = true;
+                    }
+                } else if (eventKey.Keycode == Key.Key2) {
+                    SetMessage("This is an animation test when changing the text", true);
+                }
+                
+            }
+        }
+    }
+
+    public void SetMessage(string message, bool animate = false) {
+        
+        if (animate) {
+            if (tween != null) {
+                tween.Kill();
+            }
+
+            tween = GetTree().CreateTween();
+            tween.TweenProperty(MessageLabel, "modulate:a", 0, 0.4f)
+                .SetTrans(Tween.TransitionType.Quint)
+                .SetEase(Tween.EaseType.Out);
+            tween.TweenCallback(Callable.From(() => {
+                MessageLabel.Text = message;
+
+                var tween2 = GetTree().CreateTween();
+                tween2.TweenProperty(MessageLabel, "modulate:a", 1, 0.4f)
+                    .SetTrans(Tween.TransitionType.Quint)
+                    .SetEase(Tween.EaseType.Out);
+            }));
+        } else {
+            MessageLabel.Text = message;
+        }
+    }
+
+    public void ShowDialog() {
+        if (tween != null) {
+            tween.Kill();
+        }
+
+        tween = GetTree().CreateTween();
+        tween.TweenProperty(this, "position", new Vector2(1188, Position.y), 1.2f)
+            .SetTrans(Tween.TransitionType.Quint)
+            .SetEase(Tween.EaseType.InOut);
+    }
+
+    public void HideDialog() {
+        if (tween != null) {
+            tween.Kill();
+        }
+
+        tween = GetTree().CreateTween();
+        tween.TweenProperty(this, "position", new Vector2(2000, Position.y), 1.2f)
+            .SetTrans(Tween.TransitionType.Quint)
+            .SetEase(Tween.EaseType.InOut);
+    }
+}
